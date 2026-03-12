c3abfa5787d8f454a73f4ddd06a2e1c4a838aca5
anika.dovob <anika.dovob@kakaocorp.com>
2026-03-12 12:03:53 +0900

    [ANIKA] 12, 13장 과제

---
+++ anika.dovob/src/main/java/com/example/anika/homework/chapter13/Chapter13Application.java
@@ -0,0 +1,12 @@
+package com.example.anika.homework.chapter13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.anika.homework.chapter12", "com.example.anika.homework.chapter13"})
+public class Chapter13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter13Application.class, args);
+    }
+}
